2951396e80859f1e3bc25dfe4b923022625af69d
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-03-02 23:58:30 +0530

    Made an add function

--- src/add.js
+++ src/add.js
@@ -1,6 +1,6 @@
 /*eslint-disable */
 import { Todo } from "./Todo.js";
-import deleteBtn from './delete.png'
+
 
 export const fillList = () => {
   const todoList = document.getElementById('todo-list');
@@ -13,8 +13,60 @@ export const fillList = () => {
           <input class='checkbox' type='checkbox' name='' value=''>
           <span>${item.description}</span>
           <textarea class="text-area" maxlength="30">${item.description}</textarea>
-          <img class="cancel-btn" src="${deleteBtn}">
+          <button type = "button">&CircleTimes;</button>
           `;
     todoList.appendChild(listItem);
+    const checkbox = listItem.querySelector("input");
+  const text = listItem.querySelector("span");
+  const textInput = listItem.querySelector("textarea");
+  const deleteButton = listItem.querySelector("img");
+  checkbox.addEventListener("change", () => {
+    const index = parseInt(listItem.id, 10);
+    Todo.list[index].update();
+    text.classList.toggle("complete");
+    textInput.classList.toggle("complete");
+    localStorage.setItem("todoList", JSON.stringify(Todo.list));
+  });
+  textInput.addEventListener("click", () => {
+    text.style.display = "none";
+    textInput.classList.toggle("edit-item");
+  });
+  textInput.addEventListener("keydown", (e) => {
+    text.innerHTML = textInput.value;
+
+    const index = parseInt(listItem.id, 10);
+    Todo.list[index].description = text.innerHTML;
+
+    localStorage.setItem("todoList", JSON.stringify(Todo.list));
+
+    if (e.code === "Enter") {
+      text.style.display = "block";
+      textInput.classList.toggle("edit-item");
+    }
+  });
+  image.addEventListener("click", () => {
+    const index = parseInt(listItem.id, 10);
+    Todo.list = Todo.list.filter((item) => item !== Todo.list[index]);
+    Todo.list.forEach((item, i) => { item.index = i; });
+    localStorage.setItem("todoList", JSON.stringify(Todo.list));
+    populateList();
+  });
+  if (item.complete) {
+    checkbox.checked = true;
+    text.classList = "complete";
+  }
   });
 };
+export function add(e) {
+  if (e.code === "Enter") {
+    // Create new item
+    const newItem = new Todo(this.value, false);
+
+    // Update local storage
+    localStorage.setItem("todoList", JSON.stringify(newItem.getList()));
+
+    // Update UI
+    this.value = "";
+    fillList();
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 /* eslint-disable no-unused-vars */
-import { fillList } from './add.js';
+import { fillList, add } from './add.js';
 import './index.css';
 import { Todo } from './Todo.js';
 
+const list = JSON.parse(localStorage.getItem('todoList'));
+if (list) {
+  list.forEach((item) => new Todo(item.description, item.complete));
+}
+const addInput = document.getElementById('add-input');
+addInput.addEventListener('keydown', add);
 fillList();
